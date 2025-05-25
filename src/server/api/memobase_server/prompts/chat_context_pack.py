914bf57d8abd040f8ee0e1dfe07300df5407c56d
okkband <snliu26@163.com>
2025-05-25 20:47:02 +0800

    change user prompt's xml format with markdown

--- src/server/api/memobase_server/prompts/chat_context_pack.py
+++ src/server/api/memobase_server/prompts/chat_context_pack.py
@@ -16,14 +16,13 @@ Unless the user has relevant queries, do not actively mention those memories in 
 
 
 def zh_context_prompt(profile_section: str, event_section: str) -> str:
-    return f"""<memory>
-# 以下是用户的用户画像：
+    return f"""#记忆
+## 以下是用户的用户画像：
 {profile_section}
 
-# 以下是用户的最近事件：
+## 以下是用户的最近事件：
 {event_section}
-</memory>
-除非用户有相关的需求，否则不要主动在对话中提到这些memory.
+除非用户有相关的需求，否则不要主动在对话中提到这些记忆.
 """
 
 
